a966556c26298fb36475d861de05cad7ea3906b1
hakeemshah <msmservices1846@gmail.com>
2024-01-27 15:31:26 +0530

    cors

--- mvc/server.js
+++ mvc/server.js
@@ -5,9 +5,17 @@ const userRoutes = require('./routes/userRoutes');
 const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT;
+const cors = require('cors');
+
+app.use(cors({
+    origin : "http://127.0.0.1:5500",
+    methods : ["GET","POST"],
+    allowedHeaders : ['Content-Type'],
+}));
 
 app.use(express.json())
 app.use(userRoutes);
+app.use(express.static(__dirname + "/client"));
 
 connect();
 app.listen(PORT,() => {
