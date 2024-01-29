947f71b36c1f1267ae2d1c7eadb37343c5b1ccac
hakeemshah <msmservices1846@gmail.com>
2024-01-29 13:14:01 +0530

    signup in login folder

--- login/server.js
+++ login/server.js
@@ -3,8 +3,18 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const connect = require('./db/connect');
+const userRoutes = require('./routes/userRoutes');
 //Database Connection
 connect();
+
+//Parsing JSON
+app.use(express.json());
+
+//Parsing form datas
+app.use(express.urlencoded({extended : false}));
+
+//userRoutes
+app.use(userRoutes);
 
 app.get('/test', (req, res) => {
     res.status(200).send("success");
